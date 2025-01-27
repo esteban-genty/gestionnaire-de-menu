278125513b9ae205d814e3099b0d11adbc453fa5
esteban-genty <esteban.genty@laplateforme.io>
2025-01-27 14:04:59 +0100

    Ajout de la page catégories des recettes

---
+++ categories.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Site gestion de recettes">
+    <meta name="keywords" content="Carteo, Recette, Restaurant, Gestion de recette">
+    <meta name="author" content="Estéban, Antoine, Lamine, Sébastien">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carteo - Categories</title>
+
+    <!-- Fichier styles -->
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="categories.css">
+    <link rel="stylesheet" href="style.css">
+
+
+    <!-- Police d'écriture -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php require_once(__DIR__ . '/header.php'); ?>
+    
+</body>
+</html>
