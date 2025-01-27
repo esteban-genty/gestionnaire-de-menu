d0f94556628fd1cb022a01a77f6b80b274ce303e
Antoine <antoine.leca@laplateforme.io>
2025-01-27 19:45:39 +0100

    modifications du form, de ses couleurs, animations

--- inscription.php
+++ inscription.php
@@ -6,12 +6,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles/inscription.css">
+    <link rel="stylesheet" href="./styles/header.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
     <title>Inscription</title>
 </head>
 <body>
+    <?php require_once(__DIR__ . '/header.php'); ?>
     <main>
         <section>
             <img src="ressources\logo-carteo.png" alt="logo carteo">
@@ -19,17 +21,17 @@
         <h1>Inscription</h1>
         <div class="formsection">
             <form action="" method="post">
-                <label for="">Société*</label>
+                <label for="">Société</label>
                 <input type="text" name="entreprise" id="entreprise" required>
-                <label for="">Email*</label>
+                <label for="">Email</label>
                 <input type="email" name="email" id="email" required>
-                <label for="">Prénom*</label>
+                <label for="">Prénom</label>
                 <input type="text" name="prenom" id="prenom" required>
-                <label for="">Nom*</label>
+                <label for="">Nom</label>
                 <input type="text" name="nom" id="nom" required>
-                <label for="">Mot de passe*</label>
+                <label for="">Mot de passe</label>
                 <input type="password" name="password" id="password" required>
-                <label for="">Confirmer mot de passe*</label>
+                <label for="">Confirmer mot de passe</label>
                 <input type="password" name="password" id="password" required>
                 <button type="submit" name="submitbutton">S'inscrire</button>
             </form>
