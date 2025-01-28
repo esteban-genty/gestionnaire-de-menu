e68844f8b61d3f153a1e0ec393d802c9536e7095
Live Yu Peng Sébastien <sebastien.live-yu-peng@laplateforme.io>
2025-01-28 12:56:00 +0100

    header commun

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
             <li><a href="accueil.php"><h5>Carteo</h5></a></li>
         </ul>
         <ul class="nav-right">
-            <li><a href="moncompte.html">Mon Compte</a></li>
+            <li><a href="moncompte.html">Mon Compte</a></li>.
         </ul>
     </nav>
 </header>
