1f9a3a66b40e22ca7e6389f89341a90e6eb44520
esteban-genty <esteban.genty@laplateforme.io>
2025-01-28 10:50:00 +0100

    Ajout du fichier start.php pour se connecter à la BDD

---
+++ start.php
@@ -0,0 +1,16 @@
+<?php
+
+    // Information pour se connecter
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname = 'carteo';
+
+    // Connexion base de donnés avec PDO
+    $bddPDO = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    // Affiche les erreurs
+    $bddPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "<p>Connexion réussie</p>";
+
+?>
