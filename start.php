612a94ab7515ce3b14c268dfd92a227f8de17bff
esteban-genty <esteban.genty@laplateforme.io>
2025-01-30 14:06:49 +0100

    Ajout de session start dans start.php

--- start.php
+++ start.php
@@ -1,5 +1,6 @@
 <?php
 
+
     // Information pour se connecter
     $host = 'localhost';
     $username = 'root';
@@ -8,8 +9,11 @@
 
     // Connexion base de donnés avec PDO
     $bddPDO = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // echo "<p><strong>Connexion réussie</strong></p>";
 
     // Affiche les erreurs
     $bddPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    session_start();
+    var_dump(value : $_SESSION['user'])
 ?>
